9d3e912bdddb1f585cd9fb46fa96b91c66908012
PatriciaFlores1 <fb21010@ues.edu.sv>
2024-03-05 09:15:55 -0600

    Tutoria 02-27/02/2024 ciclo de vida de la actividad

---
+++ UNIDAD 1/PROYECTOS/Tutoria0227022024/app/src/main/java/com/example/tutoria02_27_02_2024/MainActivity.kt
@@ -0,0 +1,43 @@
+package com.example.tutoria02_27_02_2024
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        Log.i("cicloVida", "Ingreso al onCreate()") //referenia en .xml
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.i("cicloVida", "Ingreso al onStart()")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.i("cicloVida", "Ingreso al onResume()")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.i("cicloVida", "Ingreso al onPause()")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.i("cicloVida", "Ingreso al onStop()")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Log.i("cicloVida", "Ingreso al onRestart()")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.i("cicloVida", "Ingreso al onDestroy()")
+    }
+}
